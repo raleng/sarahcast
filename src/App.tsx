37cd9e980884c9ea5fc3d10e024a6bf14b444697
raleng <github@ralfengbers.com>
2019-09-22 23:59:38 +0200

    api call works

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,72 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+//import logo from "./logo.svg";
+import "./App.css";
+import ForecastCard from "./components/ForecastCard";
 
-const App: React.FC = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const API_KEY = process.env.REACT_APP_TIDE_SUNRISE_API_KEY
+const API_USER = process.env.REACT_APP_TIDE_SUNRISE_API_USER
+const API_URL = "https://raleng.pythonanywhere.com/sunrise-tide-api/v1.0"
+
+
+class App extends Component {
+  state = {
+    dates: Array<Data>()
+  };
+
+  componentDidMount() {   
+    let headers = new Headers();
+    headers.append('Authorization', 'Basic ' + btoa(API_USER + ':' + API_KEY));
+
+    fetch(API_URL, { method: 'GET', headers: headers })
+      .then(response => response.json())
+      .then(data => {
+        this.setState( { dates: data })
+        
+        for (let [key, value] of Object.entries(data)) {
+          console.log(`k: ${key}, v: ${value}`)
+          let d = new STData;
+          d.date = key;
+          d.sunrise = Object.entries(data[key])[0][1] as string;
+          let foo = Object.entries(data[key])[1][1]
+          
+
+        }
+  
+      
+      })
+      .catch(console.log);
+  }
+
+  render() {
+    return (
+      <section className="section">
+        <div className="container">
+          <h1 className="title">Sarahcast!</h1>
+          <p className="subtitle">
+            This is going to be the new home of the <strong>Sarahcast!</strong>
+          </p>
+          <div><ForecastCard/></div>
+        </div>
+      </section>
+    );
+  }
+}
+
+interface Data {
+  date: {
+    sunrise: string;
+    tides: {
+      high: string[],
+      low: string[]
+    }
+  }
+}
+
+class STData {
+  date: string = "";
+  sunrise: string = "";
+  tides_low: string[] = [];
+  tides_high: string[] = [];
 }
 
 export default App;
---
+++ src/components/ForecastCard.tsx
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+import styled from "styled-components";
+
+class ForecastCard extends Component {
+  render() {
+  
+    return <div><h1>F</h1>
+    <p>Here goes something.</p>
+    </div>;
+  }
+}
+
+export default ForecastCard;
